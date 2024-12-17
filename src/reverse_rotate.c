e2311820db3473adbefd635f67325b43fb689318
BjornSauralt <maxime.garsault@gmail.com>
2024-12-17 16:59:21 +0100

    commit

---
+++ src/reverse_rotate.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgarsaul <mgarsaul@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-12-17 15:32:15 by mgarsaul          #+#    #+#             */
+/*   Updated: 2024-12-17 15:32:15 by mgarsaul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+static void	reverse_rotate(t_noeud **stack)
+{
+	t_noeud	*last;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	last = find_last(*stack);
+	last->prev->next = NULL;
+	last->next = *stack;
+	last->prev = NULL;
+	*stack = last;
+	last->next->prev = last;
+}
+
+void	rra(t_noeud **a, bool print)
+{
+	reverse_rotate(a);
+	if (!print)
+		ft_printf("rra\n");
+}
+
+void	rrb(t_noeud **b, bool print)
+{
+	reverse_rotate(b);
+	if (!print)
+		ft_printf("rrb\n");
+}
+
+void	rrr(t_noeud **a, t_noeud **b, bool print)
+{
+	reverse_rotate(a);
+	reverse_rotate(b);
+	if (!print)
+		ft_printf("rrr\n");
+}
---
+++ src/rotate.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_commands                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgarsaul <mgarsaul@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-12-17 15:29:29 by mgarsaul          #+#    #+#             */
+/*   Updated: 2024-12-17 15:29:29 by mgarsaul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+static void	rotate(t_noeud **stack)
+{
+	t_noeud	*last_noeud;
+
+	if (!*stack || !(*stack)->next)
+		return ;
+	last_noeud = find_last(*stack);
+	last_noeud->next = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	last_noeud->next->prev = last_noeud;
+	last_noeud->next->next = NULL;
+}
+
+void	ra(t_noeud **a, bool print)
+{
+	rotate(a);
+	if (!print)
+		ft_printf("ra\n");
+}
+
+void	rb(t_noeud **b, bool print)
+{
+	rotate(b);
+	if (!print)
+		ft_printf("rb\n");
+}
+
+void	rr(t_noeud **a, t_noeud **b, bool print)
+{
+	rotate(a);
+	rotate(b);
+	if (!print)
+		ft_printf("rr\n");
+}
